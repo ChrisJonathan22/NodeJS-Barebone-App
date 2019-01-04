d7d4eb54940ee797b8f13e57e521f92c5e151f7e
Chris Jonathan <christophereko@hotmail.fr>
2019-01-04 14:25:45 +0000

    I have added some comments.

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-const http = require('http');
-const server = http.createServer();
-const port = 3000;
+const http = require('http');   // Call in the builtin http module into this file.
+const server = http.createServer(); // Create a http server.
+const port = 3000;  // Create a variable and store the port number.
 
-server.on('request', (req,res) => {
-    let body = [];
-    req.on('data', chunk => {
-        body.push(chunk);
+server.on('request', (req,res) => { // On server request do this.
+    let body = [];  // Create an empty array.
+    req.on('data', chunk => {   // Upon receiving data do this.
+        body.push(chunk);   // Save or push the data into the array.
     });
     req
-    .on('end', () => {
-        body = body.concat.toString();
+    .on('end', () => {  // When there's no more data coming through do this.
+        body = body.concat.toString();  // Concatenate all the content of the array and turn it into a string.
     })
-    .on('error', () => {
-        res.statusCode = 400;
-        res.end();
+    .on('error', () => {    // On request error do this.
+        res.statusCode = 400;   // Send the status code 400.
+        res.end();  // End the response.
     });
-    res.on('error', err => {
-        console.err(err);
+    res.on('error', err => {    // On response error do this.
+        console.err(err);   // Log the error.
     });
-    res.write( "Hello World!");
-    res.end();
-}).listen(port, () => {
-    console.log(`Server listening on port: ${port}`);
+    res.write( "Hello World!"); // On a successful request, send the response.
+    res.end();  // End the response.
+}).listen(port, () => { // Listen on port 3000.
+    console.log(`Server listening on port: ${port}`);   // Once a connection has been established log this message.
 });
